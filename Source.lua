9b9a240fa78a25c157bb4aa83a98be701845a753
xG3rsi <107426913+xG3rsi@users.noreply.github.com>
2022-06-13 23:32:37 +0200

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -687,7 +687,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=6138670105",
+		Image = "http://www.roblox.com/asset/?id=5582730217",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
